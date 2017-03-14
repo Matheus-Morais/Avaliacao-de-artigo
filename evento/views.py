712241665620d07318798a7148604aaa5d867b56
Matheus <matheus.silva.morais01@gmail.com>
2017-03-13 21:02:36 -0300

    5 interação

--- evento/views.py
+++ evento/views.py
@@ -7,9 +7,9 @@ def AllAvaliacoes(request):
     lista_avaliacao = avaliacaoArtigo.objects.all()
     retorno = ''
     for artigo in lista_avaliacao:
-        retorno += "<li>" + str(artigo.id_artigo) + " / " + str(artigo.id_avaliador) + " / " + str(artigo.nota) + " / "+ str(artigo.data) +"</li>"
+        retorno += "Id artigo" + str(artigo.id_artigo) + " -- id avaliador = " + str(artigo.id_avaliador) + " -- nota = " + str(artigo.nota) + " -- data = "+ str(artigo.data) +"<br/>"
     return HttpResponse(retorno)
 
 def avaliacaoX(request, id):
     avalicao = avaliacaoArtigo.objects.get(pk = id)
-    return HttpResponse("id artigo = " +str(avalicao.id_artigo)+ " -- id avaliador = "+str(avalicao.id_avaliador)+" -- avaliacao = "+str(avalicao.nota))
+    return HttpResponse("id artigo = " +str(avalicao.id_artigo)+ " -- id avaliador = "+str(avalicao.id_avaliador)+" -- avaliacao = "+str(avalicao.nota)+ " -- data = " +str(avalicao.data))
